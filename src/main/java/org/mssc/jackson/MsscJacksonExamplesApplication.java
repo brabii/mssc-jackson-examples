b978bc4e10b051ae74688974ed925b67e257b427
brabii <collectorsgarbages@gmail.com>
2021-01-07 13:48:22 +0100

    créer le projet

---
+++ src/main/java/org/mssc/jackson/MsscJacksonExamplesApplication.java
@@ -0,0 +1,12 @@
+package org.mssc.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJacksonExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscJacksonExamplesApplication.class, args);
+	}
+}
---
+++ src/test/java/org/mssc/jackson/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mssc.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
